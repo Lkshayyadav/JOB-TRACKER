2f1532acab1b451e7813ce3834355eaa2ca1a9ea
Lakshay Yadav <lakshay.yadav@jobtrack.dev>
2026-07-14 14:52:48 +0530

    fixes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -27,7 +27,13 @@ const initDB = async () => {
 initDB();
 
 // Middlewares
-app.use(cors());
+app.use(cors(
+  {  
+origin: process.env.FRONTEND_URL,
+  credentials: true,
+  }
+
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
